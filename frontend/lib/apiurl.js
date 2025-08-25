224cd5de4e28cb75e970dc0f8f8035b3f97e8687
sukhairkar <sukantkhairkar@gmail.com>
2025-08-25 09:16:55 +0530

    Update apiurl.js
    added /api at the end

--- frontend/lib/apiurl.js
+++ frontend/lib/apiurl.js
@@ -1,2 +1,2 @@
 const useLocal = false;
-export const API_URL = useLocal?"http://localhost:5000/api": "https://event-management-website-1-pn9y.onrender.com";
+export const API_URL = useLocal?"http://localhost:5000/api": "https://event-management-website-1-pn9y.onrender.com/api";
